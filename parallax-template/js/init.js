071bc3fb823c4fb6979e47e2e804337f94728834
Sanmc84 <moreloscs200884@gmail.com>
2018-10-14 23:23:24 -0500

    Cambios de portafolio

---
+++ parallax-template/js/init.js
@@ -0,0 +1,11 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+function menuactive(){
+  
+}
+})(jQuery); // end of jQuery name space
